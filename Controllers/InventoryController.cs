0c348aebfcdd8b5de763dff38f48970bb64163ec
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2024-10-17 02:32:38 +0700

    apply template v1 (#2)

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lemoo_pos.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
