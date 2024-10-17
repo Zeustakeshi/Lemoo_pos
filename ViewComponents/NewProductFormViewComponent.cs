8a8441a28709d8e8927eb860c66859e36b664daf
Minh Hiếu <gaconmvp2312@gmail.com>
2024-10-17 22:38:57 +0700

    new product view

---
+++ ViewComponents/NewProductFormViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lemoo_pos.ViewComponents
+{
+    public class NewProductFormViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
