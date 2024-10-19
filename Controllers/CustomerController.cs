63b2fe0fda17c211463b1f39540053922f992546
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2024-10-19 22:43:28 +0700

    new product ui (#4)
    * Update Customer/Index.cshtml
    
    * Update  view Customer index README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: LeganSar44 <hoangslevan@gmail.com>
    Co-authored-by: Eurus44 <129585402+LeganSar44@users.noreply.github.com>

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lemoo_pos.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult NewCustomer() { return View(); }
+
+
+     
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 <img src = "https://res.cloudinary.com/dymmvrufy/image/upload/v1728847601/toomeet_play/channel_avatars/jssf1oh2ytlxjrn9lphy.png"/>
+
+[<img src = "https://imgur.com/f1DEzft"/>
+(https://i.imgur.com/f1DEzft.png)](https://i.imgur.com/f1DEzft.png)
