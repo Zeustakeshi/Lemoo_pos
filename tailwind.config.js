6396b56e3f625acb864ce7901e31fc0129f4e8f9
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2024-10-28 23:31:41 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     content: ["./Views/**/*.{cshtml,razor}", "./wwwroot/js/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            
+        },
     },
     plugins: [],
 };
